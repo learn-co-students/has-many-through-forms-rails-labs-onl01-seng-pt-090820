e04c3229d278ab741869fe0133a31dc1a0641e5f
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-01-18 21:29:56 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,27 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :comments, reject_if: :comment_blank
+
+  # accepts_nested_attributes_for :categories
+
+  def categories_attributes=(category_attributes)
+    
+      category_attributes.values.each do |category_attribute|
+        # byebug
+        if category_attribute[:name].present?
+          category = Category.find_or_create_by(category_attribute)
+          if !self.categories.include?(category)
+            self.post_categories.build(category: category)
+          end
+        end
+      end
+    
+  end
+
+  def comment_blank(attributes) 
+    attributes[:content].blank?
+  end
+
 
 end
