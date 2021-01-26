d6d89e2c637fa899c9207337962dc2648fb8af23
DavidRMorphew <morphewd@umich.edu>
2021-01-26 17:42:28 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  
+  def user_attributes=(user_attr)
+    # binding.pry
+    if !user_attr.values.all? {|val| val.blank? }
+      user = User.find_or_create_by(user_attr)
+      self.user = user
+    end
+  end
 
+  def to_s
+    "#{self.user.username} says: #{self.content}"
+  end
 end
