5bc131c84c8c08da46b2f3062bcb5fa08222f3b8
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-01-27 17:42:59 -0800

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,17 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  #accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attr)
+    if !user_attr.values.all? {|val| val.blank? }
+      user = User.find_or_create_by(user_attr)
+      self.user = user
+    end
+  end
+
+  def to_s
+    "#{self.user.username} says: #{self.content}"
+  end
 
 end
