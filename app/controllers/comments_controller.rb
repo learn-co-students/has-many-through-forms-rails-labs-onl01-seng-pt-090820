1e2a035cc120cbb8c4f4bab43c4cdcfdb8bc0eaa
Carlj0666 <carlj1881@yahoo.com>
2021-01-29 13:44:58 -0800

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,13 @@
 class CommentsController < ApplicationController
 
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
   def create
     comment = Comment.create(comment_params)
     redirect_to comment.post
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, comments_attributes: [:content, :username], category_ids:[], categories_attributes: [:name])
   end
 end
 
