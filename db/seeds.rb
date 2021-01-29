96192a2549020d0e535ce26946d048156d88c2a0
Carlj0666 <carlj1881@yahoo.com>
2021-01-28 18:46:53 -0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+categories = Category.create([{ name: 'Food' }, { name: 'History' }])
+comments = Comment.create([{ content: 'comments about food' }, { content: 'comments about history' }])
+# post_categories = PostCategory.create([{ name: 'post CAT1' }, { name: 'post CAT2' }])
+posts = Post.create([{ title: 'Food Times', content: 'some words about food' }, { title: 'History Times', content: 'some words about history' }])
+users = User.create([{ username: 'User1234', email: 'user1234@email.com'}, { username: 'User5678', email: 'user5678@email.com'}])
