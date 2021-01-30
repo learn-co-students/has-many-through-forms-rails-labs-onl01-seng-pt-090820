e76f54c5a66bcbe2939cf3eac133084d637bb4b8
Carlj0666 <carlj1881@yahoo.com>
2021-01-29 20:48:09 -0800

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment < ActiveRecord::Base
 
   def user_attributes=(user_attributes)
     user_attributes.values.each do |user_attribute|
-      self.user= User.find_or_create_by(username: user_attributes[:username])
+      self.user= User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
    
     end
   end
