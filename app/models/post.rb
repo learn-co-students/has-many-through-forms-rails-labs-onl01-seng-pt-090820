ccea698a5e439c74bca09635e8bdf2a4928ab6bb
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-04 01:26:13 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,17 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(attributes)
+    attributes.each do |k, v|
+      if !v["name"].empty?
+        category = Category.find_or_create_by(name: v["name"])
+        self.categories << category
+      end
+    end
+    
+  end
 
 
 end
